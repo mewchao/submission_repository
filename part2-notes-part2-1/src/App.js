9c1719706f4e5477b074870f4c075bc7f0420507
you junchao <2835190741@qq.com>
2024-03-26 13:27:55 +0800

    json-server

--- part2-notes-part2-1/src/App.js
+++ part2-notes-part2-1/src/App.js
@@ -1,13 +1,37 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Note from './components/Note'
 
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+const App = () => {
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState(
     'a new note...'
   )
   const [showAll, setShowAll] = useState(true)
+
+  const hook = () => {
+    // console.log('effect')
+    // axios
+    //   .get('http://localhost:3001/notes')
+    //   .then(response => {
+    //     console.log('promise fulfilled')
+    //     setNotes(response.data)
+    //   })
+    console.log('effect')
+
+    const eventHandler = (response) => {
+      console.log('promise fulfilled')
+      setNotes(response.data)
+    }
+  
+    const promise = axios.get('http://localhost:3001/notes')
+    promise.then(eventHandler)
+  }
+  
+  useEffect(hook, [])
+
+  console.log('render', notes.length, 'notes')
 
   const handleNoteChange = (event) => {
     console.log(event.target.value)
--- part2-notes-part2-1/src/index.js
+++ part2-notes-part2-1/src/index.js
@@ -1,25 +1,9 @@
 import ReactDOM from "react-dom/client"
-
 import App from "./App"
 
-const notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
-  },
-]
+// axios.get('http://localhost:3001/notes').then(response => {
+//   const notes = response.data
+//   ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes} />)
+// })
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <App notes={notes} />
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
