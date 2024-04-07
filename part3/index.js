0a31f8c003849f3cc0ea1af699f7438246ececc5
you junchao <2835190741@qq.com>
2024-04-07 17:01:56 +0800

    REST-notes

--- part3/index.js
+++ part3/index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const PORT = 3001
 
+// 用于启用内置的 express.json() 中间件，以便正确解析请求体
+app.use(express.json())
+
 let notes = [
   {
     id: 1,
@@ -22,6 +25,7 @@ let notes = [
     important: true
   }
 ]
+
 // 第一个 request 参数包含 HTTP 请求的所有信息，第二个 response 参数用于定义如何对请求进行响应。
 app.get('/', (request, response) => {
 response.send('<h1>Hello World!</h1>')
@@ -29,6 +33,50 @@ response.send('<h1>Hello World!</h1>')
 
 app.get('/api/notes', (request, response) => {
 response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+  console.log(body)
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+  const note = {
+    date: new Date(),
+    id: generateId(),
+    content: body.content,
+    important: body.important || false,
+  }
+  notes = notes.concat(note)
+  response.json(note)
+})
+
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
 })
 
 app.listen(PORT, () => {
